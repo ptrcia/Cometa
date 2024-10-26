ba1bccb07196d04fd9cb69abacee7fc76c8a8f9a
Patricia S. Gracia Artero <patryechelon@gmail.com>
2024-10-26 22:11:15 +0200

    hospital scene

--- Assets/Scripts/Hospital/FinalScene.cs
+++ Assets/Scripts/Hospital/FinalScene.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 public class FinalScene : MonoBehaviour
 {
     [SerializeField] GameObject dialogue;
+    [SerializeField] Animator animOut;
+    [SerializeField] Animator animIn;
+    [SerializeField] Animator animCameraOut;
+    [SerializeField] Animator animCameraIn;
 
+
+    private void Awake()
+    {
+        animOut = GetComponent<Animator>();
+        animIn = GetComponent<Animator>();
+        animCameraOut = GetComponent<Animator>();
+        animCameraIn = GetComponent<Animator>();
+
+
+    }
 
     private void Start()
     {
@@ -14,12 +28,33 @@ public class FinalScene : MonoBehaviour
     IEnumerator SceneController()
     {
         yield return new WaitForSeconds(2);
-        //dejar la consola a un lado y mirar al medico
+
+        if (animOut != null)
+        {
+            animOut.Play("ConsoleOut");
+        }
+
+        if (animCameraOut != null)
+        {
+            animCameraOut.Play("ConsoleOut");
+        }
+
         dialogue.SetActive(true);
+
         yield return new WaitForSeconds(5);
         //el niño se va
         yield return new WaitForSeconds(2);
-        //cam,biar la posicion de la camara a la consola y hace zoom
+
+        if (animOut != null)
+        {
+            animOut.Play("ConsoleIn");
+        }
+        if (animCameraIn != null)
+        {
+            animCameraIn.Play("ConsoleIn");
+        }
+
+
         yield return new WaitForSeconds(2);
         //la imagen cmabia y esta orbitando a un planeta 2 cometas
         yield return new WaitForSeconds(5);
